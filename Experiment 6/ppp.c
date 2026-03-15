43aa96c6fcc0f81da50205c1d25a57e506b25179
Sanjay Kumar S <sanjayksk1712@gmail.com>
2026-03-15 19:12:03 +0530

    Push

--- Experiment 6/ppp.c
+++ Experiment 6/ppp.c
@@ -15,3 +15,20 @@ void first(char c){
         }
     }
 }
+void follow(char c){
+    if(prod[0][0]==c){
+        printf("$ ");
+    }
+    for (int i=0;i<n;i++){
+        for(int j=0;j<strlen(prod[i];j++)){
+            if(prod[i][j]==c){
+                if (prod[i][j+1]!='\0'){
+                    printf("%c",prod[i][j+1]);
+                }
+                else if(c!=prod[i][0]){
+                    follow(prod[i][0]);
+                }
+            }
+        }
+    }
+}
