cd5e29c3bf3ee614482de67954753e795b003f37
Sanjay Kumar S <sanjayksk1712@gmail.com>
2026-03-15 20:12:01 +0530

    Push

--- Experiment 6/ppp.c
+++ Experiment 6/ppp.c
@@ -4,10 +4,10 @@
 char prod[10][10];
 int n;
 void first(char c){
-    for (int i=0;i<n;i++){
+    for(int i=0;i<n;i++){
         if(prod[i][0]==c){
             if(!isupper(prod[i][2])){
-                printf("%s",prod[i][2]);
+                printf("%c",prod[i][2]);
             }
             else{
                 first(prod[i][2]);
@@ -16,19 +16,41 @@ void first(char c){
     }
 }
 void follow(char c){
-    if(prod[0][0]==c){
+    if(prod[0][0]==c)
         printf("$ ");
-    }
-    for (int i=0;i<n;i++){
-        for(int j=0;j<strlen(prod[i];j++)){
+    
+    for(int i=0;i<n;i++){
+        for(int j=2;j<strlen(prod[i]);j++){
             if(prod[i][j]==c){
-                if (prod[i][j+1]!='\0'){
-                    printf("%c",prod[i][j+1]);
-                }
-                else if(c!=prod[i][0]){
+                if(prod[i][j+1]!='\0')
+                    printf("%c ",prod[i][j+1]);
+                else if(c!=prod[i][0])
                     follow(prod[i][0]);
-                }
             }
         }
     }
+}
+int main(){
+    char c;
+    printf("Enter the number of productions: \n");
+    scanf("%d",&n);
+    printf("Enter the productions here!!\n");
+    for(int i=0;i<n;i++){
+        scanf("%s",prod[i]);
+    }
+    printf("\nFIRST Sets\n");
+    for(int i=0;i<n;i++){
+        c=prod[i][0];
+        printf("FIRST(%c)={",c);
+        first(c);
+        printf("}\n");
+    }
+    printf("\nFOLLOW Sets\n");
+    for(int i=0;i<n;i++){
+        c=prod[i][0];
+        printf("FOLLOW(%c)={",c);
+        follow(c);
+        printf("}\n");
+    }
+    return 0;
 }
