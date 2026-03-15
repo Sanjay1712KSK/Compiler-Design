20dfa300611c586bf2792dbaea014be54e6c2f54
Sanjay Kumar S <sanjayksk1712@gmail.com>
2026-03-15 11:42:19 +0530

    Push

--- Experiment 4/lex.yy.c
+++ Experiment 4/lex.yy.c
@@ -351,8 +351,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 6
-#define YY_END_OF_BUFFER 7
+#define YY_NUM_RULES 4
+#define YY_END_OF_BUFFER 5
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -360,27 +360,27 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[10] =
+static const flex_int16_t yy_accept[9] =
     {   0,
-        0,    0,    7,    5,    3,    4,    1,    2,    0
+        0,    0,    5,    3,    2,    1,    1,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    2,    3,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    4,    5,    5,    5,    4,    5,
-        5,    5,    4,    5,    5,    5,    5,    5,    4,    5,
-        5,    5,    5,    5,    4,    5,    5,    5,    5,    5,
-        1,    1,    1,    1,    1,    1,    4,    5,    5,    5,
+        1,    1,    1,    1,    3,    3,    3,    3,    3,    3,
+        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
+        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
+        1,    1,    1,    1,    1,    1,    3,    3,    3,    3,
 
-        4,    5,    5,    5,    4,    5,    5,    5,    5,    5,
-        4,    5,    5,    5,    5,    5,    4,    5,    5,    5,
-        5,    5,    1,    1,    1,    1,    1,    1,    1,    1,
+        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
+        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
+        3,    3,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -397,31 +397,29 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[6] =
+static const YY_CHAR yy_meta[4] =
     {   0,
-        1,    1,    1,    1,    1
+        1,    1,    2
     } ;
 
 static const flex_int16_t yy_base[10] =
     {   0,
-        0,    0,    6,    7,    7,    7,    7,    7,    7
+        0,    0,    5,    6,    6,    0,    0,    6,    2
     } ;
 
 static const flex_int16_t yy_def[10] =
     {   0,
-        9,    1,    9,    9,    9,    9,    9,    9,    0
+        8,    1,    8,    8,    8,    9,    9,    0,    8
     } ;
 
-static const flex_int16_t yy_nxt[13] =
+static const flex_int16_t yy_nxt[10] =
     {   0,
-        4,    5,    6,    7,    8,    9,    3,    9,    9,    9,
-        9,    9
+        4,    5,    6,    7,    8,    3,    8,    8,    8
     } ;
 
-static const flex_int16_t yy_chk[13] =
+static const flex_int16_t yy_chk[10] =
     {   0,
-        1,    1,    1,    1,    1,    3,    9,    9,    9,    9,
-        9,    9
+        1,    1,    1,    9,    3,    8,    8,    8,    8
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -438,12 +436,22 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "vc.l"
-#line 2 "vc.l"
-#include <stdio.h>
-int vowels=0; int consonants=0;
-#line 446 "lex.yy.c"
-#line 447 "lex.yy.c"
+#line 1 "palindrome.l"
+#line 2 "palindrome.l"
+    #include <stdio.h>
+    #include <string.h>
+    int isPalindrome(char str[]){
+        int i=0; int j=strlen(str)-1;
+        while (i<j){
+            if(str[i]!=str[j]){
+                return 0;
+            }
+            i++; j--;
+        }
+        return 1;
+    }
+#line 454 "lex.yy.c"
+#line 455 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -660,9 +668,9 @@ YY_DECL
 		}
 
 	{
-#line 5 "vc.l"
+#line 15 "palindrome.l"
 
-#line 666 "lex.yy.c"
+#line 674 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -689,13 +697,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 10 )
+				if ( yy_current_state >= 9 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 7 );
+		while ( yy_base[yy_current_state] != 6 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -721,38 +729,33 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 6 "vc.l"
-{vowels++;}
+#line 16 "palindrome.l"
+{
+    if (isPalindrome(yytext)){
+        printf("%s is a Palindrome \n",yytext);
+    }
+    else{
+        printf("%s is not a Palindrome \n",yytext);
+    }
+}
 	YY_BREAK
 case 2:
+/* rule 2 can match eol */
 YY_RULE_SETUP
-#line 7 "vc.l"
-{consonants++;}
+#line 24 "palindrome.l"
+return 0;
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 8 "vc.l"
-
-	YY_BREAK
-case 4:
-/* rule 4 can match eol */
-YY_RULE_SETUP
-#line 9 "vc.l"
-{printf("vowels words: %d \n",vowels);
-    printf("consonants words: %d \n",consonants);
-    return 0;}
-	YY_BREAK
-case 5:
-YY_RULE_SETUP
-#line 12 "vc.l"
+#line 25 "palindrome.l"
 ;
 	YY_BREAK
-case 6:
+case 4:
 YY_RULE_SETUP
-#line 13 "vc.l"
+#line 26 "palindrome.l"
 ECHO;
 	YY_BREAK
-#line 756 "lex.yy.c"
+#line 759 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1049,7 +1052,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 10 )
+			if ( yy_current_state >= 9 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1077,11 +1080,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 10 )
+		if ( yy_current_state >= 9 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 9);
+	yy_is_jam = (yy_current_state == 8);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1757,10 +1760,10 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 13 "vc.l"
+#line 26 "palindrome.l"
 
 int main(){
-    printf("Enter the word to calculate the count;");
+    printf("Enter the word:\n");
     yylex();
     return 0;
 }
