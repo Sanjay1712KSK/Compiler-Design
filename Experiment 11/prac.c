53b503e29a725fae5f2f4fbe94ff3a664540819e
Sanjay Kumar S <sanjayksk1712@gmail.com>
2026-04-26 18:39:32 +0530

    The final Compiler Design Lab Exam

---
+++ Experiment 11/prac.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+int main(){
+    char exp[50];
+    char op[20];
+    char arg1[20][10], arg2[20][10], res[20];
+    char stack[20][10];
+    int top=-1,i,j=1,k=0,ontop=-1;
+    printf("Enter the expression :");
+    scanf("%s",exp);
+    
+}
