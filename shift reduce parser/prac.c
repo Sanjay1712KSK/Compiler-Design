9d343029a05d53fef459c89e16a716382c115a39
Sanjay Kumar S <sanjayksk1712@gmail.com>
2026-04-27 05:57:08 +0530

    The final Compiler Design Lab Exam

--- shift reduce parser/prac.c
+++ shift reduce parser/prac.c
@@ -1,3 +1,18 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+int main(){
+    char input[20],stack[20];
+    int i=0,top=0;
+    printf("Grammar is E+E|E*E|(E)|id \n");
+    printf("Enter the input string :");
+    scanf("%s",input);
+    printf("Stack\tInput\tAction\n");
+    while(1){
+        if(i<strlen(input)){
+            stack[top++]=input[i++];
+            stack[top]='\0';
+            printf("%s\t%s\tShift\n",stack,input+1);
+        }
+    }
+}
